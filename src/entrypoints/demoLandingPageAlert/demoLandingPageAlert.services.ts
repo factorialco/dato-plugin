55ed934bf2b460bc33cdce78c3d0136934e43f1e
Elena Vigo <elena.vigo@factorial.co>
2025-10-16 10:32:27 +0200

    Rename key

--- src/entrypoints/demoLandingPageAlert/demoLandingPageAlert.services.ts
+++ src/entrypoints/demoLandingPageAlert/demoLandingPageAlert.services.ts
@@ -1,6 +1,6 @@
 import { buildClient } from "@datocms/cma-client-browser";
 
-const DEMO_LANDING_PAGE_MODEL_API_KEY = "evL8wHUkSgqfKeJxwaYKxA";
+const DEMO_LANDING_PAGE_MODEL = "evL8wHUkSgqfKeJxwaYKxA";
 
 const getClient = (ctx: any) => {
   if (ctx.currentUserAccessToken) {
@@ -20,7 +20,7 @@ export const getDemoLandingPageInstances = async (ctx: any) => {
 
   return await client.items.list({
     filter: {
-      type: DEMO_LANDING_PAGE_MODEL_API_KEY,
+      type: DEMO_LANDING_PAGE_MODEL,
     },
   });
 };
